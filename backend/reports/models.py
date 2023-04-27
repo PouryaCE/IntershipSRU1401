ba418f3df8f29e4efd467fd629abb90654029e95
PouryaCE <pouryajanparvar@yahoo.com>
2023-04-28 01:20:01 +0330

    report app of users created

---
+++ backend/reports/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from accounts.models import Student
+# Create your models here.
+
+
+class Report (models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(blank=True, null=True)
+    attachments = models.FileField()
+    deadline_time = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class FormsRollCall(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name="student_to_forms")
+    enter_time = models.DateTimeField()
+    exit_time = models.DateTimeField()
+
+
+class ReportStudent (models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name="student_to_report")
+    report = models.ForeignKey(Report, on_delete=models.CASCADE, related_name="report_to_student")
+    is_done = models.BooleanField(default=False)
+
+
+class Summery (models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name="summery_to_student")
+    content = models.CharField(max_length=100)
